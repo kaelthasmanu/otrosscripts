768c48aadd711ed5de05e186688a16d778b15019
Kaelthas <39976595+kaelthasmanu@users.noreply.github.com>
2023-05-08 18:15:36 -0400

    Update observer.py

--- observer.py
+++ observer.py
@@ -15,21 +15,21 @@ file = [int(s) for s in re.findall(r'-?\d+\.?\d*', fecha)]
 class testdaemon(daemon.Daemon):
     def run(self):
         if __name__ == "__main__":
-                logging.basicConfig(filename=str(file[0]),level = logging.INFO , format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-                logger = logging.getLogger()
-                observer = Observer()
-                threads=[]
-                for i in range(len(dir)):
-                    event_handler = LoggingEventHandler()
-                    observer.schedule(event_handler, dir[i], recursive=True)
-                    threads.append(observer)
-                observer.start()
-                try:
-                    while True:
-                        time.sleep(0.1)
-                except KeyboardInterrupt:
-                    observer.stop()
-                observer.join()
+            logging.basicConfig(filename=str(file[0]),level = logging.INFO , format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+            logger = logging.getLogger()
+            observer = Observer()
+            threads=[]
+            for i in range(len(dir)):
+                event_handler = LoggingEventHandler()
+                observer.schedule(event_handler, dir[i], recursive=True)
+                threads.append(observer)
+            observer.start()
+            try:
+                while True:
+                    time.sleep(0.1)
+            except KeyboardInterrupt:
+                observer.stop()
+            observer.join()
 
 daemon = testdaemon()
 
